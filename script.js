781726992195d544fccd9364cf41a71efd32a6b0
caiosampaio894 <caiosampaio894@gmail.com>
2021-03-30 16:01:36 -0300

    Fazendo movimentação dos discos(nao finalizado)

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ for (let i = 1; i <= 3; i++){
 }
 
 const subBox1 = document.getElementById('sub-box1')
+const subBox2 = document.getElementById('sub-box2')
+const subBox3 = document.getElementById('sub-box3')
+
 
 let disk1 = document.createElement('div')
 disk1.setAttribute('id', 'disk1')
@@ -27,3 +30,40 @@ subBox1.appendChild(disk1)
 subBox1.appendChild(disk2)
 subBox1.appendChild(disk3)
 subBox1.appendChild(disk4)
+
+
+
+// handler de clique nos discos
+// 1 - quando clicar na div sub-box
+// 2 - mover o disco acima
+// 3 - mudar o estado da variável de controle
+// 4 - quando clicar em outra sub-box
+// 5 - verificar se tem algum disco menor ou maior (Math.min com width?)
+// 6 - fazer verificação de aceitação
+// 7 - depositar o disco se possível
+// 8 - mudar o estado da variável de controle novamente
+// 9 - reduzir o numero de jogadas
+
+
+// 1 - evitar que os dos iFs sejam executados simultaneamente
+let variavelDeControle = true
+let movingDisk
+
+function moveDisk1(event) {
+    movingDisk = event.target
+    if(variavelDeControle === false) {
+        subBox1.appendChild(movingDisk)
+    }
+    variavelDeControle = true
+    
+    if(variavelDeControle === true) {
+        movingDisk = subBox1.removeChild(subBox1.lastElementChild)
+        
+    }
+    variavelDeControle = false
+    console.log(variavelDeControle)
+   
+
+}
+
+box.addEventListener('click', moveDisk1)
